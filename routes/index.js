41320b62d98fc18de40e00d92f6cd4a9c44afdd8
sheenahamilton <sheenhamilton@gmail.com>
2025-09-11 19:07:28 -0600

    fix main landing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => { res.send('Sheenas Project 1'); });
+router.get('/', (req, res) => { res.send('Sheenas Project 1'); });
 
 router.use('/contacts', require('./contacts'));
 
