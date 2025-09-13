eb5465025fb4deb2b763e54db5aab3a4f8b9aa26
sheenahamilton <sheenhamilton@gmail.com>
2025-09-13 14:15:58 -0600

    Project 1 W01: Contacts

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => { res.send('Sheenas Project 1'); });
+
+router.use('/contacts', require('./contacts'));
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const mongodb = require('./data/database');
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDB((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => { console.log(`Running on port ${port}`) });
+    }
+})
