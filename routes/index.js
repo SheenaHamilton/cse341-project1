dab709a48f8a763a15bd632f3e09132eb0638201
sheenahamilton <sheenhamilton@gmail.com>
2025-09-16 05:18:40 -0600

    Project 1 W02: CORS and Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('Sheenas Project 1'); });
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
+    //#swagger.tags-['Project Introduction']
+    res.send('Sheenas Project 1');
+});
 
 router.use('/contacts', require('./contacts'));
 
